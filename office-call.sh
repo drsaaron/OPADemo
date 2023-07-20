667a9e837a7bc14f04a556aa2d1e869e21d9d333
Scott Aaron <dr_saaron@yahoo.com>
2023-07-20 15:00:56 -0500

    get token once in calls

--- office-call.sh
+++ office-call.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
 
+token=$(getBlazarToken.sh)
+
 # call for all failures as an HO user
 echo "getting all failures...."
-curl -H "Authorization: Bearer $(getBlazarToken.sh )"  http://localhost:25000/failures
+curl -H "Authorization: Bearer $token"  http://localhost:25000/failures
 
 # get a specif failure
 echo
 echo "getting failure for 1001..."
-curl -H "Authorization: Bearer $(getBlazarToken.sh )"  http://localhost:25000/failures/1001
+curl -H "Authorization: Bearer $token"  http://localhost:25000/failures/1001
+
+echo
--- user-call.sh
+++ user-call.sh
@@ -1,13 +1,17 @@
 #! /bin/sh
 
+token=$(getBlazarToken.sh -u joe)
+
 # call for all failures as an office user.  This user has access to 2 failures in 2023 and 1 failure
 # in 2022.  So this call should return 3 failures.
 echo "calling for all failures...."
-curl -H "Authorization: Bearer $(getBlazarToken.sh -u joe)"  http://localhost:25000/failures
+curl -H "Authorization: Bearer $token"  http://localhost:25000/failures
 
 # get a specific failure as an office user
 echo
 echo "calling for failure on 1001..."
-curl -H "Authorization: Bearer $(getBlazarToken.sh -u joe)"  http://localhost:25000/failures/1001
+curl -H "Authorization: Bearer $token"  http://localhost:25000/failures/1001
+
+echo
 
 
