ce67806b4f3216153b1ce9f6ac23d69a116b54f4
Scott Aaron <dr_saaron@yahoo.com>
2025-09-08 15:13:02 -0500

    get requirements.txt to work

--- relationship-server-python/buildContainer.sh
+++ relationship-server-python/buildContainer.sh
@@ -7,7 +7,10 @@ containerName=relationshipServer
 docker stop $containerName
 docker rm $containerName
 
-pip3 freeze | grep -v ansible | grep -v pydantic > requirements.txt
+#pip3 freeze | grep -v ansible | grep -v pydantic > requirements.txt
+pip3 freeze | grep -E 'pydantic|fastapi|uvicorn' > requirements.txt
+perl -i -pe 's/pydantic_core==2.39.0/pydantic_core==2.33.2/' requirements.txt
+
 ~/shell/buildDocker.sh -v $imageVersion -n $imageName -f
 
 docker run -d --name $containerName --network host -p 25001:25001 $imageName:$imageVersion
